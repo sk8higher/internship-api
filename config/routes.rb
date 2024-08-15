bdf093d7db7ddb41fb7bbe282ff439a089d83b79
Nelly Simkova <sk8high3r@ya.ru>
2024-08-16 02:48:36 +0300

    Finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :schools, only: [] do
-    resources :groups
+    resources :groups, only: :index do
+      resources :students, only: :index
+    end
   end
+
+  resources :students, only: %i[create destroy]
 end
