861e970da7ad2ab161cf15ebf07a76246caa295a
Nelly Simkova <sk8high3r@ya.ru>
2024-08-16 20:49:34 +0300

    Add seeds to db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,21 @@
 # frozen_string_literal: true
 
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+10.times do |i|
+  school = School.create!
+  group = school.groups.new(number: i, letter: 'R', students_count: 20 + i)
+  group.save
+
+  20.times do |j|
+    first_name = "Testname #{j}"
+    last_name = "Testlastname #{j}"
+    surname = "TestSurname #{j}"
+
+    Student.create!(
+      first_name:,
+      last_name:,
+      surname:,
+      group_id: group.id,
+      school_id: school.id
+    )
+  end
+end
