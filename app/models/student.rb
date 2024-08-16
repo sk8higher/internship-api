b43b5175ead1f7bd6c3ffe7bf2c7cc2e66eb63b8
Nelly Simkova <sk8high3r@ya.ru>
2024-08-17 02:06:40 +0300

    Move JWT logic to model

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,4 +7,17 @@ class Student < ApplicationRecord
   attr_readonly :id
 
   validates :first_name, :last_name, :surname, :group_id, :school_id, presence: true
+
+  def generate_access_token
+    JWT.encode(payload, ENV['JWT_SECRET'], 'HS512')
+  end
+
+  def payload
+    {
+      exp: Time.now.to_i + 60 * 360,
+      iat: Time.now.to_i,
+      iss: ENV['JWT_ISSUER'],
+      student: { id: id }
+    }
+  end
 end
