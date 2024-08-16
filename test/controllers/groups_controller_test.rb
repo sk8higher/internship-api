80daf44f6d4408547c04c78d9e44c082bbdbd097
Nelly Simkova <sk8high3r@ya.ru>
2024-08-16 19:26:48 +0300

    Add tests for both controllers

--- test/controllers/groups_controller_test.rb
+++ test/controllers/groups_controller_test.rb
@@ -2,7 +2,18 @@ require "test_helper"
 
 class GroupsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get groups_index_url
+    @school = School.create
+    @group = @school.groups.create(number: 5, letter: 'Б', students_count: 32)
+
+    get school_groups_url(@school.id)
     assert_response :success
+
+    json_response = JSON.parse(response.body)
+    assert_includes json_response['data'], {
+      'id' => @group.id,
+      'number' => @group.number,
+      'letter' => @group.letter,
+      'students_count' => @group.students_count
+    }
   end
 end
--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -1,18 +1,50 @@
 require "test_helper"
 
 class StudentsControllerTest < ActionDispatch::IntegrationTest
+  def setup
+    @school = School.create
+    @group = @school.groups.create(number: 5, letter: 'Б', students_count: 32)
+    @student = @group.students.create(
+      first_name: 'Вячеслав',
+      last_name: 'Абдурахмангаджиевич',
+      surname: 'Мухобойников-Сыркин',
+      school_id: @school.id,
+      group_id: @group.id
+    )
+  end
+
   test "should get index" do
-    get students_index_url
+    get school_group_students_url(@school.id, @group.id)
     assert_response :success
+
+    json_response = JSON.parse(response.body)
+    assert_includes json_response['data'], {
+      'id' => @student.id,
+      'first_name' => @student.first_name,
+      'last_name' => @student.last_name,
+      'surname' => @student.surname,
+      'group_id' => @student.group_id,
+      'school_id' => @student.school_id
+    }
   end
 
-  test "should get create" do
-    get students_create_url
-    assert_response :success
+  test "should post create" do
+    assert_difference 'Student.count', 1 do
+      post students_url, params: { student: @student }, as: :json
+    end
+
+    assert_response :created
   end
 
-  test "should get destroy" do
-    get students_destroy_url
+  test "should delete student" do
+    post students_url, params: { student: @student }, as: :json
+    assert_response :success
+
+    token = @response.headers['X-Auth-Token']
+    student_id = JSON.parse(@response.body)['id']
+
+    delete student_url(id: student_id), headers: { 'X-Auth-Token' => token }, as: :json
+
     assert_response :success
   end
 end
